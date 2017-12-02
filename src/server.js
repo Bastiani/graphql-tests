edebc90c1a82d7e0e2e1cf66fa02b64c98c7ede4
Rafael Bastiani <rafacdb@gmail.com>
2017-12-02 10:58:37 -0200

    mutation

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 import Koa from 'koa';
 import Router from 'koa-router';
 import koaBody from 'koa-body';
-// import graphqlHttp from 'koa-graphql';
-import graphql from 'graphql';
+import graphqlHttp from 'koa-graphql';
+// import graphql from 'graphql';
 
-import database from './database';
-import Todo from './mongoose/todo';
+import connectToDatabase from './database';
+// import Todo from './mongoose/todo';
 import schema from './schema';
 
 const app = new Koa();
@@ -15,28 +15,13 @@ app.use(koaBody({
   jsonLimit: '1kb',
 }));
 
-async function create(ctx) {
-  console.log(ctx);
-  const todoItem = new Todo({
-    itemId: ctx.request.body.itemId,
-    item: ctx.request.body.item,
-    completed: false,
-  });
-
-  try {
-    await todoItem.save();
-    console.log(`+++TodoItem saved successfully ${todoItem.item}`);
-  } catch (err) {
-    console.log(`---TodoItem save failed ${err}`);
-  }
-}
-
-router.post('/', (req, res) => {
-  graphql(schema, req.body).then((result) => {
-    create(result);
-    res.send(result);
-  });
-});
+router.all(
+  '/graphql',
+  graphqlHttp({
+    schema,
+    graphiql: process.env.NODE_ENV !== 'production',
+  }),
+);
 // router.post('/post', create);
 // router.get('/list', graphqlHttp({ schema, pretty: true }));
 
@@ -46,7 +31,7 @@ app.use(async (ctx) => {
   ctx.body = 'Hello World';
 });
 
-database()
+connectToDatabase()
   .then((res) => {
     console.log(res);
     console.log('listen');
